daf3a4b70cb4524d1d067b2c442696b3a8e9240f
anthoserv <anthoserv@anthoserv.anthoserv>
2020-05-18 11:17:33 -0500

    Se agrego comentarios en la interface 09837254

--- src/main/java/com/anthoserv/security/SaveFile.java
+++ src/main/java/com/anthoserv/security/SaveFile.java
@@ -15,28 +15,28 @@ import java.io.InputStream;
  * @author anthoserv
  */
 public class SaveFile {
-   
-    public static  SaveFile newInstance() {
+
+    public static SaveFile newInstance() {
         return new SaveFile();
     }
-    
+
     /**
-     * 
+     *
      * @param directorio
      * @param nombreArchivo
      * @param inputStream
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
-    public String crearDirectorio(String directorio,String nombreArchivo,InputStream inputStream) throws Exception {
-        FileOutputStream fileOutputStream ;
+    public String crearDirectorio(String directorio, String nombreArchivo, InputStream inputStream) throws Exception {
+        FileOutputStream fileOutputStream;
         try {
             File f = new File(".");
             String url = f.getCanonicalPath();
-            String urlFinal = url + File.separator +"welcome-content"+ directorio;
+            String urlFinal = url + File.separator + "welcome-content" + directorio;
             java.io.File directorioInicial = new File(urlFinal);
             if (!directorioInicial.exists()) {
-                directorioInicial.mkdir();  
+                directorioInicial.mkdirs();
             }
             if (!directorioInicial.canWrite()) {
                 directorioInicial.setWritable(true);
@@ -58,29 +58,30 @@ public class SaveFile {
                 fileOutputStream.write(buffer, 0, bulk);
                 fileOutputStream.flush();
             }
-             fileOutputStream.close();
+            fileOutputStream.close();
             inputStream.close();
             return archivo;
         } catch (FileNotFoundException ex) {
-          throw  new Exception(ex.getMessage());
+            throw new Exception(ex.getMessage());
         }
-       
+
     }
-/**
-     * 
+
+    /**
+     *
      * @param directorio
      * @param nombreArchivo
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
-    public String crearDirectorioString(String directorio,String nombreArchivo) throws Exception {
+    public String crearDirectorioString(String directorio, String nombreArchivo) throws Exception {
         try {
             File f = new File(".");
             String url = f.getCanonicalPath();
             String urlFinal = url + File.separator + directorio;
             java.io.File directorioInicial = new File(urlFinal);
             if (!directorioInicial.exists()) {
-                directorioInicial.mkdir();  
+                directorioInicial.mkdir();
             }
             if (!directorioInicial.canWrite()) {
                 directorioInicial.setWritable(true);
@@ -90,11 +91,20 @@ public class SaveFile {
             }
             directorioInicial.list();
             String archivo = urlFinal + File.separator + nombreArchivo;
-            
+
             return archivo;
         } catch (FileNotFoundException ex) {
-          throw  new Exception(ex.getMessage());
-        } 
-       
+            throw new Exception(ex.getMessage());
+        }
+
+    }
+
+    public String extensionFile(String fileName) {
+        String extension = "";
+        int i = fileName.lastIndexOf('.');
+        if (i >= 0) {
+            extension = fileName.substring(i + 1);
+        }
+        return "."+extension;
     }
 }
